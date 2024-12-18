d0eb7d9147cb970f833601c492f01dc0812ea42b
Djay <dannana.jayanth@gmail.com>
2024-12-18 10:53:11 -0500

    cart selection added

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -1,27 +1,27 @@
-import React, { Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import {
   privateRoutes,
   publicRoutes,
   pirvateSections,
   Products,
-} from "./routesData.js";
-import "bootstrap/dist/css/bootstrap.min.css";
+} from './routesData.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
-import "./routes.css"
+import './routes.css';
 function RoutesComponent() {
   // Accessing the logged-in state from the Redux store
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
   return (
     <BrowserRouter>
-    <div className="app-container">
-      <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-        <main className="main-content">
-          <Routes>
+      <div className="app-container">
+        <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          <main className="main-content">
+            <Routes>
               {publicRoutes.map(({ path, element: Element }) => (
                 <Route key={path} path={path} element={<Element />} />
               ))}
@@ -44,10 +44,10 @@ function RoutesComponent() {
                   ))}
                 </>
               )}
-          </Routes>
-        </main>
-      </Suspense>
-      <Footer />
+            </Routes>
+          </main>
+        </Suspense>
+        <Footer />
       </div>
     </BrowserRouter>
   );
--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { removeFromCart, updateQuantity } from '../../store/cartSlice'; // Import the necessary actions
-import axios from "axios";
-import "./cart.css";
+import axios from 'axios';
+import './cart.css';
+import { useEffect, useState } from 'react';
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  // State to manage selected items
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  useEffect(() => {
+    setSelectedItems(cartItems.map((item) => item.productId));
+  }, [cartItems]);
+
+  // Handle checkbox toggle
+  const handleCheckboxChange = (productId) => {
+    if (selectedItems.includes(productId)) {
+      setSelectedItems((prevSelected) =>
+        prevSelected.filter((id) => id !== productId)
+      );
+    } else {
+      setSelectedItems((prevSelected) => [...prevSelected, productId]);
+    }
+  };
+
   // Calculate total price
-  const totalPrice = cartItems.reduce((total, item) => total + parseFloat(item.price.replace('$', '')) * item.quantity, 0);
+  const totalPrice = cartItems
+    .filter((item) => selectedItems.includes(item.productId))
+    .reduce(
+      (total, item) =>
+        total + parseFloat(item.price.replace('$', '')) * item.quantity,
+      0
+    );
 
   const handleContinueToPayment = async () => {
     // Check if the user has an address before proceeding to payment
@@ -30,9 +54,13 @@ const Cart = () => {
     }
   };
 
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
   return (
     <div className="cart-container">
-      <h1 className='yourCart'>Your Cart</h1>
+      <h1 className="yourCart">Your Cart</h1>
       {cartItems.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
@@ -40,16 +68,57 @@ const Cart = () => {
           <ul className="cart-items">
             {cartItems.map((item, index) => (
               <li key={index} className="cart-item">
-                  <img src={item.image} alt={item.productName} className="cart-item-image" />
+                <input
+                  type="checkbox"
+                  checked={selectedItems.includes(item.productId)}
+                  onChange={() => handleCheckboxChange(item.productId)}
+                />
+                <img
+                  src={item.image}
+                  alt={item.productName}
+                  className="cart-item-image"
+                />
                 <div className="cart-item-details">
                   <h2>{item.productName}</h2>
-                  <p><strong>Description:</strong> {item.description}</p>
-                  <p><strong>Price:</strong> {item.price}</p>
+                  <p>
+                    <strong>Description:</strong> {item.description}
+                  </p>
+                  <p>
+                    <strong>Price:</strong> {item.price}
+                  </p>
                   <div className="quantity-controls">
-                    <button onClick={() => dispatch(updateQuantity({ productId: item.productId, quantity: item.quantity - 1 }))} disabled={item.quantity <= 1}>-</button>
+                    <button
+                      onClick={() =>
+                        dispatch(
+                          updateQuantity({
+                            productId: item.productId,
+                            quantity: item.quantity - 1,
+                          })
+                        )
+                      }
+                      disabled={item.quantity <= 1}
+                    >
+                      -
+                    </button>
                     <span> {item.quantity} </span>
-                    <button onClick={() => dispatch(updateQuantity({ productId: item.productId, quantity: item.quantity + 1 }))}>+</button>
-                    <button onClick={() => dispatch(removeFromCart(item.productId))} className='btn btn-danger'>Remove</button>
+                    <button
+                      onClick={() =>
+                        dispatch(
+                          updateQuantity({
+                            productId: item.productId,
+                            quantity: item.quantity + 1,
+                          })
+                        )
+                      }
+                    >
+                      +
+                    </button>
+                    <button
+                      onClick={() => dispatch(removeFromCart(item.productId))}
+                      className="btn btn-danger"
+                    >
+                      Remove
+                    </button>
                   </div>
                 </div>
               </li>
@@ -57,7 +126,12 @@ const Cart = () => {
           </ul>
           <div className="total-price">
             <h3>Total Price: ${totalPrice.toFixed(2)}</h3>
-            <button className="btn btn-primary" onClick={handleContinueToPayment}>Proceed to Checkout</button>
+            <button
+              className="btn btn-primary"
+              onClick={handleContinueToPayment}
+            >
+              Proceed to Checkout
+            </button>
           </div>
         </>
       )}
